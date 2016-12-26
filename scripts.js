60f6c1eeb627d52b7250236e67fa36ab5560d96d
Mord Maman <jaime.mord.maman@gmail.com>
2016-12-26 16:13:38 +0000

    figuring out how to make onKeyDown work

--- scripts.js
+++ scripts.js
@@ -1,9 +1,8 @@
-window.onload = function() {
-    var Path = paper.Path();
+
     for(var x=0; x< 1000; x+=100){
     for(var y=0; y< 1000; y+=100){
-        new Path.Circle(new Point(x, y), 10).fillColor="yellow";
+       var path =  new Path.Circle(new Point(x, y), 10).fillColor="yellow";
         }
     }
-}
+
 	
